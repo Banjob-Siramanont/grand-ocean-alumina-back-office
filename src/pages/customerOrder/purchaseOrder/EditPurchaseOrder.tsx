466b33d9b60c66317d82374b0a6a6f072bddb837
Banjob Siramanont <Banjob_Siramanont@hotmail.com>
2025-07-03 18:46:44 +0700

    create editPurchaseOrder

--- src/pages/customerOrder/purchaseOrder/EditPurchaseOrder.tsx
+++ src/pages/customerOrder/purchaseOrder/EditPurchaseOrder.tsx
@@ -1,7 +1,58 @@
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+// Components
+import CardPrimary from '../../../common/card/CardPrimary';
 import TopicOfPage from '../../../common/topic/TopicOfPage';
+import BasicInformation from './components/BasicInformation';
+import ProductList from './components/ProductList';
+import SpecialProductList from './components/SpecialProductList';
+import ActionButtons from '../../components/ActionButtons';
+
+// Types
+import type { RootState } from '../../../store/Store';
+import { useEffect } from 'react';
 
 export default function EditPurchaseOrder() {
+
+    const searchParams = new URLSearchParams(location.search);
+    const id = searchParams.get('id');
+
+    const {
+        selectedCustomerCompany,
+        poNumber,
+        orderDate,
+        expiredDate,
+        shippingAddress,
+        productDatas,
+    } = useSelector((state: RootState) => state.purchaseOrderDataStateValue);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        console.log(`call api with ID: ${id}`);
+    }, []);
+
+    console.log({
+        selectedCustomerCompany,
+        poNumber,
+        orderDate,
+        expiredDate,
+        shippingAddress,
+        productDatas,
+    });
+
     return (
-        <TopicOfPage text='แก้ไขข้อมูลใบ PO' />
+        <>
+            <TopicOfPage text='แก้ไขข้อมูลใบ PO' />
+            <CardPrimary>
+                <BasicInformation />
+                <ProductList />
+                <SpecialProductList />
+                <ActionButtons actionText='ถัดไป' onClick={() => navigate(`/edit/verify-purchase-order?id=${id}`)} />
+            </CardPrimary>
+
+        </>
     )
 }
+
