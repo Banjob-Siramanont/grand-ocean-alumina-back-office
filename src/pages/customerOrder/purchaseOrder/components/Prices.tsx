0bc1956dffd3528005f0df7f72ae6cd789f1d883
Banjob Siramanont <Banjob_Siramanont@hotmail.com>
2025-07-03 17:27:36 +0700

    style: edit color

--- src/pages/customerOrder/purchaseOrder/components/Prices.tsx
+++ src/pages/customerOrder/purchaseOrder/components/Prices.tsx
@@ -17,7 +17,7 @@ export default function Prices() {
     const price = calculateOrderTotals(productDatas, specialProductDatas, productOptionDatas);
 
     return (
-        <div className='text-right text-themeColor dark:text-white'>
+        <div className='text-right dark:text-white'>
             <div className='text-right'>ยอดสั่งซื้อ : {currency(price.totalPrice)} บาท</div>
             <div className='text-right'>ภาษีมูลค่าเพิ่ม : {currency(price.totalVat)} บาท</div>
             <div className='text-right'>รวมทั้งสิ้่น : {currency(price.grandTotal)} บาท</div>
