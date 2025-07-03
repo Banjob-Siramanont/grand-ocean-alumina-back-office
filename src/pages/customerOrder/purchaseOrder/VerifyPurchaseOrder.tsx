528cb3c644b6c80278bc521d0cd5f9611c68d4a6
Banjob Siramanont <Banjob_Siramanont@hotmail.com>
2025-07-03 17:29:06 +0700

    add summaryInformation and summaryTable to VerifyPurchaseOrder

--- src/pages/customerOrder/purchaseOrder/VerifyPurchaseOrder.tsx
+++ src/pages/customerOrder/purchaseOrder/VerifyPurchaseOrder.tsx
@@ -1,7 +1,38 @@
+import { useNavigate } from 'react-router-dom';
+import OutlinedButton from '../../../common/button/OutlinedButton';
+import CardPrimary from '../../../common/card/CardPrimary';
 import TopicOfPage from '../../../common/topic/TopicOfPage';
+import SummaryInformation from './components/SummaryInformation';
+import SummaryTable from './components/SummaryTable';
+import ContainedButton from '../../../common/button/ContainedButton';
 
 export default function VerifyPurchaseOrder() {
+
+    const navigate = useNavigate();
+
     return (
-        <TopicOfPage text='ตรวจสอบข้อมูลใบ PO' />
+        <>
+            <TopicOfPage text='ตรวจสอบข้อมูลใบ PO' />
+            <CardPrimary>
+                <SummaryInformation />
+                <SummaryTable />
+                <div className='flex justify-end items-center gap-x-2 mt-8'>
+                    <OutlinedButton
+                        text='แก้ไขข้อมูล'
+                        textColor='text-themeColor'
+                        bgColor='bg-themeColor'
+                        borderColor='border-themeColor'
+                        onClick={() => navigate(-1)}
+                    />
+                    <ContainedButton
+                        text='เพิ่มใบ PO'
+                        bgColor='bg-themeColor'
+                        borderColor='border-themeColor'
+                        onClick={() => console.log('Add Purchase Order')}
+                    />
+                </div>
+            </CardPrimary>
+        </>
+
     )
 }
