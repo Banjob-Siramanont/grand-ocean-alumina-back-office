8733a0cb1c0cbb7108217e06a68b74db063973cf
Banjob Siramanont <Banjob_Siramanont@hotmail.com>
2025-07-03 17:43:16 +0700

    fix: last child has border right when there is no action button

--- src/common/table/components/TableRow.tsx
+++ src/common/table/components/TableRow.tsx
@@ -13,7 +13,7 @@ export default function TableRow({ idx, tHeadDatas, formattedDataKey, element }:
 
     return (
         <td
-            className='py-2 px-1.5 border-r dark:border-black border-lightGrey'
+            className={`py-2 px-1.5 ${idx === tHeadDatas.length - 1 ? 'border-r-0' : 'border-r'} dark:border-black border-lightGrey`}
             style={{ textAlign: tHeadDatas[idx].cssTextAlign }}
         >
             {keyName !== '-' ? element[keyName] : keyName}
