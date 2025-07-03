fbb4d917a8d6a14ab4285fd0fdfea085ca039e58
Banjob Siramanont <Banjob_Siramanont@hotmail.com>
2025-07-03 09:28:18 +0700

    delete unuse file

--- src/types/store/vehicleBrand/vehicleBrandSliceTypes.ts
+++
@@ -1,9 +0,0 @@
-export type VehicleBrandData = {
-    vehicleEngName: string;
-    vehicleThaiName: string;
-};
-
-export type Payload<Key extends keyof VehicleBrandData> = {
-    value: VehicleBrandData[Key];
-    variableName: Key;
-}
