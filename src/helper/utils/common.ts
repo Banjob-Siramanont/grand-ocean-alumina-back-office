c6c60a6c47768a878b9c8b792b73b3a7987621a6
Banjob Siramanont <Banjob_Siramanont@hotmail.com>
2025-07-03 15:56:50 +0700

    create calculateOrderTotals function in utility

--- src/helper/utils/common.ts
+++ src/helper/utils/common.ts
@@ -1,3 +1,5 @@
+import type { ProductData, ProductOptionData, SpecialProductData } from "../../types/store/customerOrder/purchaseOrder/purchaseOrderSliceTypes";
+
 export const getCookies = (name: string) => {
     let cookieArr = document.cookie.split(';');
     for (let i = 0; i < cookieArr.length; i++) {
@@ -19,4 +21,51 @@ export function deleteAndReassignId<T extends { id: number }>(
         ...item,
         id: index + 1,
     }));
+}
+
+export function calculateOrderTotals(
+    productDatas: ProductData[],
+    specialProductDatas: SpecialProductData[],
+    productOptions: ProductOptionData[],
+) {
+    const VAT_FRACTION = 0.07; // 7% VAT
+
+    // Calculate regular products
+    const totalProductPrice = productDatas.reduce((total, product) => {
+        const productOption = productOptions.find(option => option._id === product.product_id);
+        return total + (productOption ? productOption.price * (Number(product.amount) || 0) : 0);
+    }, 0);
+
+    const totalProductVat = productDatas.reduce((total, product) => {
+        const productOption = productOptions.find(option => option._id === product.product_id);
+        return total + (productOption ? productOption.vat * (Number(product.amount) || 0) : 0);
+    }, 0);
+
+    // Calculate special products
+    const totalSpecialProductPrice = specialProductDatas.reduce((total, product) => {
+        const productOption = productOptions.find(option => option._id === product.product_id);
+        const sumPrice = (productOption?.price || 0) + (Number(product.addedPrice) || 0);
+        return total + (productOption ? sumPrice * (Number(product.amount) || 0) : 0);
+    }, 0);
+
+    const totalSpecialProductVat = specialProductDatas.reduce((total, product) => {
+        const productOption = productOptions.find(option => option._id === product.product_id);
+        const sumVatPrice = (productOption?.vat || 0) + (VAT_FRACTION * Number(product.addedPrice) || 0);
+        return total + (productOption ? sumVatPrice * (Number(product.amount) || 0) : 0);
+    }, 0);
+
+    // Calculate totals
+    const totalPrice = totalProductPrice + totalSpecialProductPrice;
+    const totalVat = totalProductVat + totalSpecialProductVat;
+    const grandTotal = totalPrice + totalVat;
+
+    return {
+        totalProductPrice,
+        totalProductVat,
+        totalSpecialProductPrice,
+        totalSpecialProductVat,
+        totalPrice,
+        totalVat,
+        grandTotal
+    };
 }
