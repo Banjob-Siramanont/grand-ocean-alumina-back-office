2b1693c22c4e982f34424205840c00e7e06c22ab
Banjob Siramanont <Banjob_Siramanont@hotmail.com>
2025-07-04 15:44:23 +0700

    create claim/addClaim

--- src/pages/customerOrder/claim/AddClaim.tsx
+++ src/pages/customerOrder/claim/AddClaim.tsx
@@ -1,7 +1,21 @@
+import CardPrimary from '../../../common/card/CardPrimary';
 import TopicOfPage from '../../../common/topic/TopicOfPage';
+import BasicInformation from './components/BasicInformation';
+import RepairProductList from './components/RepairProductList';
+import ReplaceProductList from './components/ReplaceProductList';
+import ReturnProductList from './components/ReturnProductList';
 
 export default function AddClaim() {
+
     return (
-        <TopicOfPage text='ส่งซ่อม / คืน / เปลี่ยน' />
+        <>
+            <TopicOfPage text='ส่งซ่อม / คืน / เปลี่ยน' />
+            <CardPrimary>
+                <BasicInformation />
+                <RepairProductList />
+                <ReturnProductList />
+                <ReplaceProductList />
+            </CardPrimary>
+        </>
     )
 }
